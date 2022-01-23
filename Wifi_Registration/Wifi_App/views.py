88fc4da6d90da1a5425d31ac75ace64d2da5af57
lerrylerry <laungayanjohnlerry@gmail.com>
2022-01-23 17:28:05 +0800

    Lerry
    Views.py

--- Wifi_Registration/Wifi_App/views.py
+++ Wifi_Registration/Wifi_App/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'Wifi_App/FACULTY.html')
+    return render(request, 'Wifi_App.HOMEPAGE.html')
 
 def faculty(request):
-    return render(request, 'Wifi_App/index.html')
+    return render(request, 'Wifi_App/FACULTY.html')
 
 def student(request):
     return render(request, 'Wifi_App/STUDENT.html')
+
+def admin(request):
+    return render(request, 'Wifi_App/ADMINLOGIN.html')
+
+def dataReq(request):
+    return render(request, 'Wifi_App/DATA REQUEST.html')
+
+def dataFac(request):
+    return render(request, 'Wifi_App/DATAFACULTY.html')
+
+def success(request):
+    return render(request, 'Wifi_App/success.html')
