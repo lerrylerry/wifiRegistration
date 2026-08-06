07adc1b1e817326d70c7769fe9df3c60c492f7bc
lerrylerry <laungayanjohnlerry@gmail.com>
2026-08-06 18:59:39 +0800

    Fix settings.py middleware order for whitenoise

--- Wifi_Registration/Wifi_Registration/settings.py
+++ Wifi_Registration/Wifi_Registration/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #for caching CSS, JS, Bootstrap Files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django_session_timeout.middleware.SessionTimeoutMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware' #for caching CSS, JS, Bootstrap Files
 ]
 
 ROOT_URLCONF = 'Wifi_Registration.urls'
